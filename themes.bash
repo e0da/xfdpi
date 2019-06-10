df2ba8a868300df9f34f384af14ac987dc8a9436
Justin Force <justin.force@gmail.com>
2019-06-10 12:52:56 -0700

    Extract themes to separate file

---
+++ themes.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#               label  panel_size  wm_theme     cursor_size  dpi
+export SMALL=(  small  32          Numix        16           96   )
+export BIG=(    big    48          "Numix-154"  42           144  )
