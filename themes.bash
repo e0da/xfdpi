d566510585de08816331b5963ad80821588269ee
Justin Force <justin.force@gmail.com>
2019-06-11 22:59:04 -0700

    Manage tray_icon_size

--- themes.bash
+++ themes.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#               label  panel_size  wm_theme     cursor_size  dpi
-export SMALL=(  small  32          Numix        16           96   )
-export BIG=(    big    48          "Numix-154"  42           144  )
+#               label  panel_size  tray_icon_size  wm_theme     cursor_size  dpi
+export SMALL=(  small  32          32              Numix        16           96   )
+export BIG=(    big    48          48              "Numix-154"  42           144  )
