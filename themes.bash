138f348fd28e3c1f1a44e4f975c0d05100ec6ed6
Justin Force <justin.force@gmail.com>
2019-06-13 11:37:42 -0700

    Change tray icon size

--- themes.bash
+++ themes.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #               label  panel_size  tray_icon_size  wm_theme     cursor_size  dpi
-export SMALL=(  small  32          32              Numix        16           96   )
-export BIG=(    big    48          48              "Numix-154"  42           144  )
+export SMALL=(  small  32          28              Numix        16           96   )
+export BIG=(    big    48          40              "Numix-154"  42           144  )
